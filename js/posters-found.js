14de8d039d13b6559c6e5718a225b5752949af74
George <george.figgs@gmail.com>
2016-02-26 17:13:55 -0700

    init
    foundation, fancybox version

---
+++ js/posters-found.js
@@ -0,0 +1,407 @@
+// us foundation instread of bootstrap posters.js
+
+var posters = [
+
+	{
+		"title": "Art Deco Dead",
+		"description": "Some Description",
+		"link": "#",
+		"date": "10/07/2006",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/ArtDecoDead.jpg",
+		"id": 1
+	},
+
+	{
+		"title": "Backstage Pass",
+		"description": "Some Description",
+		"link": "#",
+		"date": "10/24/1998",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/BackstagePass.jpg",
+		"id": 2
+	},
+
+	{
+		"title": "Blue Eyes",
+		"description": "Some Description",
+		"link": "#",
+		"date": "03/21/2009",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/BlueEyes.jpg"
+		,
+		"id": 3
+	},
+
+	{
+		"title": "Celtic Dead",
+		"description": "Some Description",
+		"link": "#",
+		"date": "04/19/1997",
+		"artist": "Chris O'Riley, George Bain, Arleigh",
+		"orient": "p",
+		"imgSrc": "img/CelticDead.jpg",
+		"id": 4
+	},
+
+	{
+		"title": "Dark Star",
+		"description": "",
+		"link": "#",
+		"date": "10/04/2003",
+		"artist": "Chris O'Riley, Gustave Dore",
+		"orient": "p",
+		"imgSrc": "img/DarkStar.jpg",
+		"id": 5
+	},
+
+	{
+		"title": "Dead &amp; Beyond",
+		"description": "",
+		"link": "#",
+		"date": "03/24/2007",
+		"artist": "Chris O'Riley, B. Thomas, A. Owsley",
+		"orient": "p",
+		"imgSrc": "img/DeadBeyond.jpg",
+		"id": 6
+	},
+
+
+	{
+		"title": "Dead Through the Years",
+		"description": "",
+		"link": "#",
+		"date": "03/19/2011",
+		"artist": "Chris O'Riley, Bob Marks",
+		"orient": "h",
+		"imgSrc": "img/DeadThroughtheYears.jpg",
+		"id": 7
+	},
+
+	{
+		"title": "Fractal Skull",
+		"description": "",
+		"link": "#",
+		"date": "03/08/2008",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/FractalSkull.jpg",
+		"id": 8
+	},
+
+	{
+		"title": "Jerry Garcia Memorial",
+		"description": "",
+		"link": "#",
+		"date": "11/04/1995",
+		"artist": "Judy Strahota, Chris O'Riley",
+		"orient": "h",
+		"imgSrc": "img/JerryGarciaMemorial.jpg",
+		"id": 9
+	},
+
+	{
+		"title": "Jerry Garcia Tribute",
+		"description": "",
+		"link": "#",
+		"date": "10/16/2010",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/JerryGarciaTribute.jpg",
+		"id": 9
+	},
+
+	{
+		"title": "Maya Dead",
+		"description": "",
+		"link": "#",
+		"date": "04/03/2004",
+		"artist": "Chris O'Riley",
+		"orient": "h",
+		"imgSrc": "img/MayaDead.jpg",
+		"id": 10
+	},
+
+	{
+		"title": "OZO",
+		"description": "",
+		"link": "#",
+		"date": "10/13/2012",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/OZO.jpg",
+		"id": 11
+	},
+
+	{
+		"title": "Pigpen",
+		"description": "",
+		"link": "#",
+		"date": "03/20/2010",
+		"artist": "Chris O'Riley, Bob Marks",
+		"orient": "p",
+		"imgSrc": "img/Pigpen.jpg",
+		"id": 12
+	},
+
+	{
+		"title": "Skeletons on the Mount",
+		"description": "",
+		"link": "#",
+		"date": "10/22/1988",
+		"artist": "Bruce Campbell, Gustave Dore",
+		"orient": "p",
+		"imgSrc": "img/SkeletonsontheMount.jpg",
+		"id": 13
+	},
+
+	{
+		"title": "The Band",
+		"description": "",
+		"link": "#",
+		"date": "10/16/1999",
+		"artist": "Chris O'Riley",
+		"orient": "h",
+		"imgSrc": "img/TheBand.jpg",
+		"id": 14
+	},
+
+	{
+		"title": "The Bus",
+		"description": "",
+		"link": "#",
+		"date": "10/18/1997",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/TheBus.jpg",
+		"id": 15
+	},
+
+	{
+		"title": "The Carrion Crow",
+		"description": "",
+		"link": "#",
+		"date": "04/24/1999",
+		"artist": "Arleigh, Chris O'Riley",
+		"orient": "h",
+		"imgSrc": "img/TheCarrionCrow.jpg",
+		"id": 16
+	},
+
+	{
+		"title": "The Dragon",
+		"description": "",
+		"link": "#",
+		"date": "04/20/2002",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/TheDragon.jpg",
+		"id": 17
+	},
+
+	{
+		"title": "The Flag",
+		"description": "",
+		"link": "#",
+		"date": "3/23/2013",
+		"artist": "Chris O'Riley",
+		"orient": "h",
+		"imgSrc": "img/TheFlag.jpg",
+		"id": 18
+	},
+
+	{
+		"title": "The Jester",
+		"description": "",
+		"link": "#",
+		"date": "10/14/2000",
+		"artist": "Arleigh, Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/TheJester.jpg",
+		"id": 19
+	},
+
+	{
+		"title": "The Mask",
+		"description": "",
+		"link": "#",
+		"date": "10/17/2015",
+		"artist":  "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/TheMask.jpg",
+		"id": 20
+	},
+
+	{
+		"title": "The Phil Zone",
+		"description": "",
+		"link": "#",
+		"date": "04/19/2003",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/ThePhilZone.jpg",
+		"id": 21
+	},
+
+	{
+		"title": "The Pipe",
+		"description": "",
+		"link": "#",
+		"date": "10/15/2011",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/ThePipe.jpg",
+		"id": 22
+	},
+
+	{
+		"title": "The Train",
+		"description": "",
+		"link": "#",
+		"date": "09/20/2008",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/TheTrain.jpg",
+		"id": 23
+	},
+
+	{
+		"title": "The Zodiac",
+		"description": "",
+		"link": "#",
+		"date": "03/03/2012",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/TheZodiac.jpg",
+		"id": 24
+	},
+
+	{
+		"title": "Tie Dye Dead",
+		"description": "",
+		"link": "#",
+		"date": "04/27/1996",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/TieDyeDead.jpg",
+		"id": 25
+	},
+
+	{
+		"title": "Truckin",
+		"description": "",
+		"link": "#",
+		"date": "10/27/2001",
+		"artist": "Martin Dadisman, Chris O'Riley",
+		"orient": "h",
+		"imgSrc": "img/Truckin.jpg",
+		"id": 26
+	},
+
+	{
+		"title": "Ultimate Dead",
+		"description": "",
+		"link": "#",
+		"date": "03/15/2004",
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/UltimateDead.jpg",
+		"id": 27
+	},
+
+	{
+		"title": "Uncle Dead",
+		"description": "",
+		"link": "#",
+		"date": "04/20/2007",
+		"artist": "Terry Kishiyama",
+		"orient": "p",
+		"imgSrc": "img/UncleDead.jpg",
+		"id": 28
+	},
+
+	{
+		"title": "Wanted",
+		"description": "",
+		"link": "#",
+		"date": "10/18/2014",
+		"artist": "Chris O'Riley, Bob Marks",
+		"orient": "p",
+		"imgSrc": "img/Wanted.jpg",
+		"id": 29
+	},
+
+{
+	"title": "Celtic Circle",
+	"description": "",
+	"link": "#",
+	"date": "03-12-2016",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/Celtic Circle.jpg",
+	"id": 30
+}
+
+];
+
+
+var header = {
+		"title": "Dead Poster Text",
+		"description": "",
+		"link": "#",
+		"artist": "Chris O'Riley",
+		"orient": "h",
+		"imgSrc": "img/DeadPosterText.JPG"
+	};
+
+
+var foo = 	{
+		"title": "Backstage Pass",
+		"description": "Some Description",
+		"link": "#",
+		"date": new Date("10/24/1998"),
+		"artist": "Chris O'Riley",
+		"orient": "p",
+		"imgSrc": "img/BackstagePass.jpg"
+	};
+
+
+function buildPosters(posters, id) {
+
+	for (var i = 0; i < posters.length; i++) {
+
+		var poster = posters[i];
+		var title = poster.title;
+		var artist = poster.artist;
+		var date = new Date(poster.date);
+		date = date.toDateString()
+
+		// build img attributes, fancybox classes
+		var img = '<img class="thumbnail" src="' + poster.imgSrc + '">';
+		var anchor = '<a href="' + poster.imgSrc + '" class="fancybox" rel="gallery" title=" '
+		+ title + '"'
+		+ '>' + img + '</a></div>';
+
+		var nameHTML = '<div class="text-center callout secondary"><h4>' 
+		+ title + '</h4><p>'
+		+ date + '<br><strong><em>' 
+		+ artist 
+		+ '</em></strong></p></div>';
+
+		// create new column div with element
+		var elem = document.createElement('div');
+		elem.className = 'column';
+		elem.innerHTML = anchor + nameHTML;
+
+		$(id).append(elem);
+	}
+}
+
+
+$(document).ready(function () {
+	buildPosters(posters, "#posters");
+	console.log("Support Independent Community Radio with your donation now at www.kgnu.org!");
+});
---
+++ js/posters.js
@@ -0,0 +1,371 @@
+// posters.js
+
+var posters = [
+
+{
+	"title": "Art Deco Dead",
+	"description": "Some Description",
+	"link": "#",
+	"date": "10-07-2006",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/ArtDecoDead.jpg"
+},
+
+{
+	"title": "Backstage Pass",
+	"description": "Some Description",
+	"link": "#",
+	"date": "10-24-1998",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/BackstagePass.jpg"
+},
+
+{
+	"title": "Blue Eyes",
+	"description": "Some Description",
+	"link": "#",
+	"date": "03-21-2009",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/BlueEyes.jpg"
+},
+
+{
+	"title": "Celtic Dead",
+	"description": "Some Description",
+	"link": "#",
+	"date": "04-19-1997",
+	"artist": "Chris O'Riley, George Bain, Arleigh",
+	"orient": "p",
+	"imgSrc": "img/CelticDead.jpg"
+},
+
+{
+	"title": "Dark Star",
+	"description": "",
+	"link": "#",
+	"date": "10-04-2003",
+	"artist": "Chris O'Riley, Gustave Dore",
+	"orient": "p",
+	"imgSrc": "img/DarkStar.jpg"
+},
+
+{
+	"title": "Dead &amp; Beyond",
+	"description": "",
+	"link": "#",
+	"date": "03-24-2007",
+	"artist": "Chris O'Riley, B. Thomas, A. Owsley",
+	"orient": "p",
+	"imgSrc": "img/DeadBeyond.jpg"
+},
+
+
+{
+	"title": "Dead Through the Years",
+	"description": "",
+	"link": "#",
+	"date": "03-19-2011",
+	"artist": "Chris O'Riley, Bob Marks",
+	"orient": "h",
+	"imgSrc": "img/DeadThroughtheYears.jpg"
+},
+
+{
+	"title": "Fractal Skull",
+	"description": "",
+	"link": "#",
+	"date": "03-08-2008",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/FractalSkull.jpg"
+},
+
+{
+	"title": "Jerry Garcia Memorial",
+	"description": "",
+	"link": "#",
+	"date": "11-04-1995",
+	"artist": "Judy Strahota, Chris O'Riley",
+	"orient": "h",
+	"imgSrc": "img/JerryGarciaMemorial.jpg"
+},
+
+{
+	"title": "Jerry Garcia Tribute",
+	"description": "",
+	"link": "#",
+	"date": "10-16-2010",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/JerryGarciaTribute.jpg"
+},
+
+{
+	"title": "Maya Dead",
+	"description": "",
+	"link": "#",
+	"date": "04-03-2004",
+	"artist": "Chris O'Riley",
+	"orient": "h",
+	"imgSrc": "img/MayaDead.jpg"
+},
+
+{
+	"title": "OZO",
+	"description": "",
+	"link": "#",
+	"date": "10-13-2012",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/OZO.jpg"
+},
+
+{
+	"title": "Pigpen",
+	"description": "",
+	"link": "#",
+	"date": "03-20-2010",
+	"artist": "Chris O'Riley, Bob Marks",
+	"orient": "p",
+	"imgSrc": "img/Pigpen.jpg"
+},
+
+{
+	"title": "Skeletons on the Mount",
+	"description": "",
+	"link": "#",
+	"date": "10-22-1988",
+	"artist": "Bruce Campbell, Gustave Dore",
+	"orient": "p",
+	"imgSrc": "img/SkeletonsontheMount.jpg"
+},
+
+{
+	"title": "The Band",
+	"description": "",
+	"link": "#",
+	"date": "10-16-1999",
+	"artist": "Chris O'Riley",
+	"orient": "h",
+	"imgSrc": "img/TheBand.jpg"
+},
+
+{
+	"title": "The Bus",
+	"description": "",
+	"link": "#",
+	"date": "10-18-1997",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/TheBus.jpg"
+},
+
+{
+	"title": "The Carrion Crow",
+	"description": "",
+	"link": "#",
+	"date": "04-24-1999",
+	"artist": "Arleigh, Chris O'Riley",
+	"orient": "h",
+	"imgSrc": "img/TheCarrionCrow.jpg"
+},
+
+{
+	"title": "The Dragon",
+	"description": "",
+	"link": "#",
+	"date": "04-20-2002",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/TheDragon.jpg"
+},
+
+{
+	"title": "The Flag",
+	"description": "",
+	"link": "#",
+	"date": "3-23-2013",
+	"artist": "Chris O'Riley",
+	"orient": "h",
+	"imgSrc": "img/TheFlag.jpg"
+},
+
+{
+	"title": "The Jester",
+	"description": "",
+	"link": "#",
+	"date": "10-14-2000",
+	"artist": "Arleigh, Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/TheJester.jpg"
+},
+
+{
+	"title": "The Mask",
+	"description": "",
+	"link": "#",
+	"date": "10-17-2015",
+	"artist":  "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/TheMask.jpg"
+},
+
+{
+	"title": "The Phil Zone",
+	"description": "",
+	"link": "#",
+	"date": "04-19-2003",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/ThePhilZone.jpg"
+},
+
+{
+	"title": "The Pipe",
+	"description": "",
+	"link": "#",
+	"date": "10-15-2011",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/ThePipe.jpg"
+},
+
+{
+	"title": "The Train",
+	"description": "",
+	"link": "#",
+	"date": "09-20-2008",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/TheTrain.jpg"
+},
+
+{
+	"title": "The Zodiac",
+	"description": "",
+	"link": "#",
+	"date": "03-03-2012",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/TheZodiac.jpg"
+},
+
+{
+	"title": "Tie Dye Dead",
+	"description": "",
+	"link": "#",
+	"date": "4-27-1996",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/TieDyeDead.jpg"
+},
+
+{
+	"title": "Truckin",
+	"description": "",
+	"link": "#",
+	"date": "10-27-2001",
+	"artist": "Martin Dadisman, Chris O'Riley",
+	"orient": "h",
+	"imgSrc": "img/Truckin.jpg"
+},
+
+{
+	"title": "Ultimate Dead",
+	"description": "",
+	"link": "#",
+	"date": "03-15-2004",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/UltimateDead.jpg"
+},
+
+{
+	"title": "Uncle Dead",
+	"description": "",
+	"link": "#",
+	"date": "04-20-2007",
+	"artist": "Terry Kishiyama",
+	"orient": "p",
+	"imgSrc": "img/UncleDead.jpg"
+},
+
+{
+	"title": "Wanted",
+	"description": "",
+	"link": "#",
+	"date": "10-18-2014",
+	"artist": "Chris O'Riley, Bob Marks",
+	"orient": "p",
+	"imgSrc": "img/Wanted.jpg"
+},
+
+{
+	"title": "Celtic Circle",
+	"description": "",
+	"link": "#",
+	"date": "03-12-2016",
+	"artist": "Chris O'Riley",
+	"orient": "p",
+	"imgSrc": "img/Wanted.jpg"
+}
+
+];
+
+var banner = {
+	"title": "Dead Poster Text",
+	"description": "",
+	"link": "#",
+	"date": "date",
+	"artist": "Chris O'Riley",
+	"orient": "h",
+	"imgSrc": "img/DeadPosterText.JPG"
+};
+
+function buildPosters(posters, id) {
+	// sort by date, with most recent on top?
+
+	for (var i = 0; i < posters.length; i++) {
+
+		var poster = posters[i];
+
+		var divElem = '<div class="col-lg-3 col-md-4 col-xs-6 thumb">';
+
+		var aElem = '<a class="thumbnail" href="#">';
+		var img = '<img class="img-responsive" src="' + poster.imgSrc + '">';
+		var anchor = '<a class="thumbnail" href="' + poster.link + '">' + img + '</a></div>';
+
+		// build caption
+		var name = poster.title;
+		var artist = poster.artist;
+		var date = poster.date;
+
+		var nameHTML = '<div class="text-center panel"><div class="bg-info panel-body"><h3>' + poster.title + '</h3>'
+		+ '<p>' + poster.date + '<br>' + poster.artist + '</p>'
+		+ '</div></div>';
+
+		var elem = document.createElement('div');
+		elem.className = 'col-lg-3 col-md-4 col-xs-6 thumb';
+		elem.innerHTML = anchor + nameHTML;
+
+		// if (poster.orient === 'p') {
+		// 	$('#posters-big').append(elem);
+		// } else {
+		// 	$('#posters-small').append(elem);
+		// }
+
+		// add poster class, see if foundation
+		// works better with rows of diff size
+		// posters
+		$('#posters').append(elem);
+	}
+}
+
+$(document).ready(function () {
+	buildPosters(posters, "#posters");
+	console.log("Support Independent Community Radio with your donation now at www.kgnu.org!");
+
+});
